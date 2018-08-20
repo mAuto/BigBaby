d33f22631da3b0cfc5e467755f296a416ce5361d
郝天 <1973356502@qq.com>
2018-08-20 23:29:55 +0800

    LiveData

--- app/src/main/java/com/mauto/bigbaby/arch_components/LiveData/BigLiveDataActivity.java
+++ app/src/main/java/com/mauto/bigbaby/arch_components/LiveData/BigLiveDataActivity.java
@@ -1,6 +1,11 @@
 package com.mauto.bigbaby.arch_components.LiveData;
 
+import android.arch.lifecycle.FullLifecycleObserver;
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleObserver;
+import android.arch.lifecycle.LifecycleOwner;
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.Observer;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -15,11 +20,15 @@ public class BigLiveDataActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.arch_activity_livedata);
 
-//        new LiveData<String>().observe(this, new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//
-//            }
-//        });
+        getLifecycle().addObserver(new FullLifecycleObserver(){});
+
+        new MutableLiveData<String>().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/mauto/bigbaby/arch_components/LiveData/LiveData.md
+++ app/src/main/java/com/mauto/bigbaby/arch_components/LiveData/LiveData.md
@@ -1,4 +1,8 @@
 ## LiveData
 LiveData是一个抽象类，源码不多，算上注释440行上下。
 #### LiveData的优点
-- 
+
+#### LifeCycleRegistry持有LiveData的observer，为什么？
+感知生命周期的不是LiveData，是LifeCycle和LifeCycleOwner。LiveData只是相当于一个观察者模式的管理器而已。如果LiveData没有与LifeCycle有关系的话，是不能实现根据生命周期状态callback的。
+#### LifeCycle是如何感知生命周期的？
+无论是Fragment还是Activity，肯定会在他们各自的生命周期调用改变生命周期状态的方法。Fragment/ReportFragment
