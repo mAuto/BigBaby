14dae3fb043d3c9a86976f1ed98e7e6816ede024
mAuto <2368338996@qq.com>
2018-08-22 19:39:00 +0800

    lifecycle

--- app/src/main/java/com/mauto/bigbaby/arch_components/LiveData_Lifecycle/BigLifecycleActivity.java
+++ app/src/main/java/com/mauto/bigbaby/arch_components/LiveData_Lifecycle/BigLifecycleActivity.java
@@ -5,8 +5,11 @@ import android.arch.lifecycle.GenericLifecycleObserver;
 import android.arch.lifecycle.Lifecycle;
 import android.arch.lifecycle.LifecycleOwner;
 import android.arch.lifecycle.LifecycleRegistry;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.Observer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.mauto.bigbaby.R;
@@ -21,6 +24,13 @@ public class BigLifecycleActivity extends AppCompatActivity  {
         setContentView(R.layout.arch_activity_lifecycle);
 
         getLifecycle().addObserver(mPresenter);
+
+        new MutableLiveData<String>().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+
+            }
+        });
 
     }
 }
