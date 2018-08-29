d944d819e9fb991cee8162e73ffde5785e58e5fe
郝天 <1973356502@qq.com>
2018-08-29 21:36:04 +0800

    modify conflict

--- app/src/main/java/com/mauto/bigbaby/MainActivity.java
+++ app/src/main/java/com/mauto/bigbaby/MainActivity.java
@@ -18,12 +18,12 @@ public class MainActivity extends AppCompatActivity {
         Router.jump(this, RoutPointer.DIFFUTILS);
     }
 
-<<<<<<< HEAD
+
     public void onClickErr(View view) {
         Router.jump(this, RoutPointer.ERR_MAIN);
-=======
+    }
     public void onClickLiveData(View view) {
         Router.jump(this, RoutPointer.LIVEDATA);
->>>>>>> c131c4a4cd5d29fd3a92b8eaad9f38872be72c0b
+
     }
 }
--- app/src/main/java/com/mauto/bigbaby/router/RoutPointer.java
+++ app/src/main/java/com/mauto/bigbaby/router/RoutPointer.java
@@ -1,25 +1,21 @@
 package com.mauto.bigbaby.router;
 
 import com.mauto.bigbaby.DiffUtil.BigDiffUtilsActivity;
-<<<<<<< HEAD
 import com.mauto.bigbaby.err.BigErrFragmentActivity;
 import com.mauto.bigbaby.err.BigErrMainActivity;
-=======
 import com.mauto.bigbaby.arch_components.LiveData_Lifecycle.BigLiveDataActivity;
->>>>>>> c131c4a4cd5d29fd3a92b8eaad9f38872be72c0b
+
 
 /**
  * Created by haohuidong on 18-6-11.
  */
 
 public enum RoutPointer {
-<<<<<<< HEAD
-    DIFFUTILS(BigDiffUtilsActivity.class.getName()),
+
     ERR_MAIN(BigErrMainActivity.class.getName()),
-    ERR_FRAGMENT(BigErrFragmentActivity.class.getName());
-=======
+    ERR_FRAGMENT(BigErrFragmentActivity.class.getName()),
     DIFFUTILS(BigDiffUtilsActivity.class.getName()), LIVEDATA(BigLiveDataActivity.class.getName());
->>>>>>> c131c4a4cd5d29fd3a92b8eaad9f38872be72c0b
+
 
     String mRoutTarget = "";
     RoutPointer(String rout) {
